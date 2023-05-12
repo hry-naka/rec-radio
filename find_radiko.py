c51ce4a00362025115f51173fc30ddd8b2f7a929
Hiroyuki NAKAMURA <128686246+hry-naka@users.noreply.github.com>
2023-05-12 05:21:49 +0000

    change spec of -a option

--- find_radiko.py
+++ find_radiko.py
@@ -18,13 +18,13 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    api = RadikoApi()
+    api = RadikoApi() 
     if args.area_id is None:
-        # defalut 'JP13' tokyo
-        result = api.search( keyword=args.keyword )
+        # no need to authorize, but need to identify area_id
+        authtoken, area_id = api.authorize()
+        result = api.search( keyword=args.keyword, area_id=area_id )
     else:
         result = api.search( keyword=args.keyword, area_id=args.area_id )
-    print( 'Title\tStation\tft\t')
     for d in result['data']:
         print( f"Title:{d['title']}\t", \
                f"-s {d['station_id']}\t", \
