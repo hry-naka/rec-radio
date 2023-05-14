c0819083d60950de8575163b221db7ed7b1a5633
Hiroyuki Nakamura <tel2sip@gmail.com>
2023-05-14 14:39:18 +0900

    add tfrec_radiko.py

--- tfrec_radiko.py
+++ tfrec_radiko.py
@@ -3,7 +3,9 @@
 import sys
 import argparse
 import shutil
-import mutagen
+import subprocess
+import time
+import requests
 from mutagen.mp4 import MP4, MP4Cover
 from mypkg.RadikoApi import RadikoApi
 
@@ -22,47 +24,44 @@ def get_args():
                 required=True, \
                 nargs=1, \
                 help="to time" )
-    # needed?
-    parser.add_argument('duration', \
-                metavar='duration', \
-                type=float, \
-                help='Duration(minutes)' )
     parser.add_argument('outputdir', \
                 metavar='outputdir', \
                 nargs='?', \
                 default='.' , \
                 help='Output path default:\'.\'' )
     parser.add_argument('prefix', \
-                metavar='Prefix name',\
+                metavar='Prefix-name',\
                 nargs='?', \
                 help='Prefix name for output file.' )
+    parser.add_argument( '-c', '--cleanup' , \
+                action='store_true' , \
+                help='Cleanup(remove) output file which recording is not completed.' )
     return parser.parse_args()
 #
 # Time Free record by ffmpeg.
 #
-def tf_rec( auth_token, channel, ft, to, outdir, prefix, date ):
+def tf_rec( auth_token, channel, ft, to, prefix, duration, date, outdir ):
     ffmpeg = shutil.which( 'ffmpeg' )
     if ffmpeg is None:
         print( 'This tool need ffmpeg to be installed to executable path' )
         print( 'Soryy, bye.')
         sys.exit(1)
-    url_parts = f' -i "https://radiko.jp/v2/api/ts/playlist.m3u8?station_id={channel}&ft={ft}&to={to}"'
+    url = f'https://radiko.jp/v2/api/ts/playlist.m3u8?station_id={channel}&l=15&ft={ft}&to={to}'
+    
     cmd = f'{ffmpeg} -loglevel fatal '
-    cmd += f'-headers "X-Radiko-AuthToken: {auth_token}" -i "{url_parts}" '
+    cmd += f'-headers "X-Radiko-AuthToken: {auth_token}" -i "{url}" '
     cmd += f'-acodec copy {outdir}/{prefix}_{date}.mp4'
-
     # Exec ffmpeg...? is there any reason to spec. duration?
-    p1 = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, shell=True)
-    time.sleep( duration )
-    p1.communicate(b'q')
-    time.sleep(10)
+    p1 = subprocess.Popen(cmd, shell=True)
+    p1.wait()
+    if( p1.returncode != 0 ):
+        print( p1.returncode, '\n', p1.stderr, '\n', p1.stdout )
+        sys.exit(1)
     return f'{outdir}/{prefix}_{date}.mp4'
 #
 # set program meta by mutagen for mp4 file
 #
 def set_mp4_meta( program, channel, area_id, rec_file ):
-    # Gee.. get_now() will not provide propper info.
-    program.get_now( channel )
     audio = MP4(rec_file)
     # track title
     title = program.get_title( channel, area_id )
@@ -85,38 +84,27 @@ def set_mp4_meta( program, channel, area_id, rec_file ):
 
 if __name__ == '__main__':
     args = get_args()
-    channel=args.station
-    stream_delay = 25 #second
-    duration=int(args.duration * 60) + stream_delay
-    outdir=args.outputdir
+    station = args.station[0]
     if args.prefix is None:
-        prefix=args.channel
+        prefix=station
     else:
         prefix=args.prefix
+    ft = args.fromtime[0]
+    to = args.totime[0]
     # setting date
-    date = DT.now().strftime('%Y-%m-%d-%H_%M')
+    date = f'{ft[0:4]}-{ft[4:6]}-{ft[6:8]}-{ft[8:10]}_{ft[10:12]}'
     # Construct RadikoApi
     api = RadikoApi()
     # Check whether channel is available
-    if api.is_avail( channel ) == False:
-        print( f'Specified station {channel} is not found.' )
+    if api.is_avail( station ) == False:
+        print( f'Specified station {station} is not found.' )
         sys.exit(1)
     # auhorize, get token and areaid
     auth_token, area_id = api.authorize()
     # get program meta via radiko api
-    url = get_streamurl( channel ,auth_token )
-    rec_file=live_rec( url, auth_token, prefix, duration, date, outdir )
-    '''
-    if args.timefree is None:
-        index = 1
-        rec_file=live_rec( url, auth_token, prefix, \
-                duration, date, outdir )
-    else:
-        index = 0
-        rec_file=tf_rec( auth_token, channel, ft, to, outdir, prefix, date )
-    '''
-    index = 1 #What's this?
-    set_mp4_meta( api, channel, area_id, rec_file )
+    api.load_program(station, ft, to, area_id)
+    rec_file=tf_rec( auth_token, station, ft, to, prefix, int(api.duration[0]), date, args.outputdir )
+    set_mp4_meta( api, station, area_id, rec_file )
     if( args.cleanup ):
         removeRecFile( outdir , DT.today() )
     sys.exit(0)
